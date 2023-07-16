ee54859d4d3f90067ee417d87777735b6360c529
Nalinaka <nalinaka@gmail.com>
2023-07-16 13:01:29 -0700

    Part 3 of Robofriends

---
+++ src/Robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+
+        id: 1,
+        name: 'Barriss Coffee',
+        username: 'Barista',
+        email: 'padawanbot@sw.com'
+    },  
+    {
+        id: 2,
+        name: 'Ulumi Galinara',
+        username: 'Unduli',
+        email: 'masterp@sw.com'
+
+    },
+
+    {
+        id: 3,
+        name: 'Securing anayla',
+        username: 'Ayla',
+        email: 'bluedeath@sw.com'
+
+    },
+
+    {
+        id: 4,
+        name: 'Ashotanned',
+        username: 'Soka',
+        email: 'katana@sw.com'
+
+    },
+
+    {
+        id: 5,
+        name: 'Fisto the kit',
+        username: 'Kit',
+        email: 'tailski@sw.com'
+
+    },
+
+    {
+        id: 6,
+        name: 'Palpardon',
+        username: 'Tine',
+        email: 'eviltine@sw.com'
+
+    },
+
+    {
+        id: 7,
+        name: 'Gru the Grog',
+        username: 'Baby Gu',
+        email: 'Babyg@sw.com'
+
+    },
+]
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+// import 'tachyons';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementbyId('root'));
+reportWebVitals(); 
+
+
